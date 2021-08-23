b77f3d2a59da2bd6cb9716d482737643ba6bff0a
Jiao-Liu <jiaoliu0228@gmail.com>
2021-08-23 14:45:46 -0500

    revise formate

--- js/app.js
+++ js/app.js
@@ -2,23 +2,23 @@
 
 
 const imgList=document.querySelector('.project-img-list');
-const imgArr=document.getElementsByClassName('project-img')
+const imgArr=document.getElementsByClassName('project-img');
  imgList.style.width = 640*imgArr.length+"px";
- var navDiv = document.getElementById("navDiv");
- var outer = document.getElementsByClassName("outer");
+const navDiv = document.getElementById("navDiv");
+ const outer = document.getElementsByClassName("outer");
 
  navDiv.style.left = (outer.offsetWidth - navDiv.offsetWidth)/2 + "px";
  
 
 
- var index = 0;
+ const index = 0;
 
- var allA = document.querySelectorAll("#navDiv a");
+const allA = document.querySelectorAll("#navDiv a");
 
  allA[index].style.backgroundColor = "black";
  
 
- for(var i=0; i<allA.length ; i++){
+ for(let i=0; i<allA.length ; i++){
 
      allA[i].num = i;
      allA[i].onclick = function(){
@@ -38,15 +38,11 @@ const imgArr=document.getElementsByClassName('project-img')
  function setA(){
      
      
-     for(var i=0 ; i<allA.length ; i++){
+     for(let i=0 ; i<allA.length ; i++){
          allA[i].style.backgroundColor = "";
      }
      allA[index].style.backgroundColor = "black";
- };
- 
+ }
 
 
-if (width <=320){
 
-
-}
